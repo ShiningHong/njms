165deb031c445b9a4c6c74d05d3d9158f8c5d658
wschi <1051737950@qq.com>
2018-10-03 15:45:28 +0800

    assf

--- src/com/bean/College.java
+++ src/com/bean/College.java
@@ -5,7 +5,23 @@ import com.utils.base.BaseEntity;
 public class College extends BaseEntity {
 
 	private int collegeId;//学院id
+	private int classId;//班级id
+	private String className;//班级名称
 	private String collegeName;//学院名称
+	
+	
+	public int getClassId() {
+		return classId;
+	}
+	public void setClassId(int classId) {
+		this.classId = classId;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
 	public int getCollegeId() {
 		return collegeId;
 	}
